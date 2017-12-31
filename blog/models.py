53f03c58fe99452372ac669c26e5f77a584c5a0f
Agskvortsov <agskvortsov@gmail.com>
2017-12-31 21:23:56 +0200

    Changet HTML

--- blog/models.py
+++ blog/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-	image = models.ImageField()
+#    image = models.ImageField()
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(
@@ -18,3 +18,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+	
+
+	
+#from __future__ import unicode_literals
+#from django.db import models
+#from django.contrib.auth.models import (
+#    BaseUserManager, AbstractBaseUser
+#)
+#
+## Create your models here.
+#class Photo(models.Model):
+#    img = models.Manager()
+#    title = models.CharField(max_length=200)
+#    width = models.IntegerField(default=0)
+#    height = models.IntegerField(default=0)
+#    image = models.ImageField()
+#    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+#
+#    def __unicode__(self):
+#        return self.title
