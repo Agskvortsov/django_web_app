9aa564a154ea5b07cd994bbc4227ddc31cbbb8ae
Agskvortsov <agskvortsov@gmail.com>
2018-01-07 16:33:05 +0200

    Almost final

--- site_new/urls.py
+++ site_new/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from blog import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'', include('blog.urls')),
 	# url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
