332f30667a39e71d8234f7612f394b8c9962fe19
Agskvortsov <agskvortsov@gmail.com>
2018-01-07 10:39:20 +0200

    Templates added

--- site_new/urls.py
+++ site_new/urls.py
@@ -20,4 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'', include('blog.urls')),
+	# url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
